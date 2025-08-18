292ec030aeca7b67f5cd2454088d30a50f025a4b
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2025-08-18 22:48:39 +0530

    initial commit

---
+++ MovieList/components/MovieCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const MovieCard = () => {
+  return (
+    <View>
+      <Text>MovieCard</Text>
+    </View>
+  )
+}
+
+export default MovieCard
+
+const styles = StyleSheet.create({})

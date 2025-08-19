94b1a0fee0054e01d6643fc56c1f3bcaa54ca744
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2025-08-20 00:15:21 +0530

    movie component and func.

--- MovieList/components/MovieCard.tsx
+++ MovieList/components/MovieCard.tsx
@@ -11,38 +11,36 @@ const MovieCard = ({
   release_date,
 }: Movie) => {
   return (
-    <View>
-      <Link href={`/movie/${id}`} asChild>
-        <TouchableOpacity className="flex-1">
-          <Image
-            source={{
-              uri: poster_path
-                ? `https://image.tmdb.org/t/p/w500${poster_path}`
-                : "https://placeholder.co/600x400/1a1a1a/ffffff.png",
-            }}
-            className="w-full h-52 rounded-lg"
-            resizeMode="cover"
-          />
-          <Text
-            className="text-white text-sm font-semibold mt-2"
-            numberOfLines={1}
-          >
-            {title}
-          </Text>
-          <View className="flex-row items-center justify-start gap-x-1">
+    <Link href={`/movie/${id}`as any} asChild>
+      <TouchableOpacity className="flex-1">
+        <Image
+          source={{
+            uri: poster_path
+              ? `https://image.tmdb.org/t/p/w500${poster_path}`
+              : "https://placeholder.co/600x400/1a1a1a/ffffff.png",
+          }}
+          className="w-full h-52 rounded-lg"
+          resizeMode="cover"
+        />
+        <Text
+          className="text-white text-sm font-semibold mt-2"
+          numberOfLines={1}
+        >
+          {title}
+        </Text>
+        <View className="flex-row items-center justify-between gap-x-1">
+          <View className="flex-row items-center gap-x-1">
             <Image source={icons.star} className="size-4" />
             <Text className="text-xs text-white font-bold">
               {Math.round(vote_average / 2)}
             </Text>
           </View>
-          <View className="flex-row items-center justify-between">
-            <Text className="text-xs text-light-300 font-medium mt-1">
-              {release_date?.split("-")[0] || "N/A"}
-            </Text>
-          </View>
-        </TouchableOpacity>
-      </Link>
-    </View>
+          <Text className="text-xs text-light-300 font-medium mt-1">
+            {release_date?.split("-")[0] || "N/A"}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </Link>
   );
 };
 
